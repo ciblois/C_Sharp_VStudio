26292fea46a4890745a90c3d96e446b1937c5c7c
Cinthya Blois <cinthyalblois@gmail.com>
2020-09-03 15:39:09 +0100

    number is divisible per 10, 5, 2

---
+++ Ex_2-11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ex_2_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //Criar um programa que recebe um número do teclado e mostra se ele é divisível por 10, por 5, 
+        //por 2 ou se não é divisível por nenhum destes.
+
+        Console.WriteLine("Let's check if a number is divisible per 10, 5, 2 or none.");
+
+        float number = 0.0f;
+
+            Console.WriteLine("Type the numer here: ");
+            number = Convert.ToSingle(Console.ReadLine());
+
+            if (number % 2 == 0 & number % 5 == 0)
+            {
+                Console.WriteLine("This number is divisible per 10.");
+            }
+            if (number % 5 == 0)
+            {
+                Console.WriteLine("This number is divisible per 5.");
+            }
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("This number is divisible per 2.");
+            }
+            else
+            {
+                Console.WriteLine("This number is NOT divisible per 10, 5, 2.");
+            }
+        }
+    }
+}
