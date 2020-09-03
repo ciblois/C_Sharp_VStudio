5f14d71f385d7cdac369c77a9bfa2825a2588575
Cinthya Blois <cinthyalblois@gmail.com>
2020-09-03 11:16:45 +0100

    new exercise with if

--- Ex_2-1/Program.cs
+++ Ex_2-1/Program.cs
@@ -6,7 +6,25 @@ namespace Ex_2_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Crie um programa que peça dois valores do teclado e caso a sua soma dê negativa, 
+            //multiplique-a por (-1), mostrando no ecrã o resultado final.
+
+            float var1, var2, resultado;
+
+            Console.WriteLine("Type the first numer: ");
+            var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type the second numer: ");
+            var2 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = var1 + var2;
+
+            if (resultado < 0)
+            {
+                resultado = resultado * (-1);
+            }
+
+            Console.WriteLine("The result is " + resultado);
         }
     }
 }
