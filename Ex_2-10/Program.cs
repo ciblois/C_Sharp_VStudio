91201418c559e840c08b7393e5a722d0a1b6901f
Cinthya Blois <cinthyalblois@gmail.com>
2020-09-03 12:19:59 +0100

    number multiple of 5

---
+++ Ex_2-10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex_2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crie um programa que determine se um número é múltiplo de 5.
+
+            float number;
+
+            Console.WriteLine("Let's check if a number is a multiple of 5!");
+            Console.WriteLine("Give me a number to check: ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number %5 == 0)
+            {
+                Console.WriteLine("This number is a multiple of 5!");
+            }
+            else
+            {
+                Console.WriteLine("This number is not a multiple of 5.");
+            }
+        }
+    }
+}
---
+++ Ex_2-7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crie um programa que peça ao utilizador:
+            //o O consumo médio de um automóvel por cada 100KM.
+            //o O tamanho do depósito em litros;
+            //o O preço do litro em euros;
+            //o E os quilómetros a percorrer numa viagem.
+            //Este por sua vez, deve calcular:
+                //2.7.1.Qual o gasto em combustível(litros e dinheiro);
+                //2.7.2.Quantas vezes deverá parar para abastecer;
+                //2.7.3.Quantos quilómetros será possível percorrer com cada depósito.
+
+            Console.WriteLine("Average car consumption");
+        }
+    }
+}
