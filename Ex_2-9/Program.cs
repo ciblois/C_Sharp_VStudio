8aaf5e78841d6cd759af0b1da8dc83b70a4abfd5
Cinthya Blois <cinthyalblois@gmail.com>
2020-09-03 11:37:59 +0100

    check if a number is odd or even

---
+++ Ex_2-9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex_2_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crie um programa que verifique se um número é par.
+
+            Console.WriteLine("Give me a number and I tell you if it's even.");
+            Console.WriteLine("Type the number if I'll check: ");
+
+            int number;
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("This number is even!");
+            }
+            else
+            {
+                Console.WriteLine("This number is odd.");
+            }
+        }
+    }
+}
