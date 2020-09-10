d48e01362146bb438d75e0f3d9e49caeec5e15cb
Cinthya Blois <cinthyalblois@gmail.com>
2020-09-10 09:51:50 +0100

    new exercise

---
+++ Ex_3-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = 0;
+            int i = 0;
+
+            while (num >= 0)
+            {
+                Console.WriteLine("Type one number");
+                num = Convert.ToInt16(Console.ReadLine());
+
+                num =
+                i++;
+            }
+
+        }
+    }
+}
---
+++ Ex_3-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+
+namespace Ex_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0, menor21 = 0, maior50 = 0;
+
+            while (idade != -99)
+            {
+                Console.WriteLine("Introduza a sua idade ou digite -99 para encerrar o programa: ");
+                idade = Convert.ToInt32(Console.ReadLine());
+
+                if (idade < 21 && idade > 0)
+                {
+                    menor21++;
+                }
+
+                if (idade > 50 && idade > 0)
+                {
+                    maior50++;
+                }
+            }
+
+            Console.WriteLine("\n\n Idadaes introduzidas menor que 21: " + menor21);
+            Console.WriteLine("\n Idadaes introduzidas maior que 50: " + maior50);
+        }
+    }
+}
