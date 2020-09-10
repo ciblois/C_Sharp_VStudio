d34aef503df64bb98ea3bc71b7112d4b97c53bf5
Cinthya Blois <cinthyalblois@gmail.com>
2020-09-10 11:58:54 +0100

    Ex_4-1 - while

--- Ex_3-3/Program.cs
+++ Ex_3-3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Ex_3_3
 {
@@ -7,16 +8,19 @@ namespace Ex_3_3
         static void Main(string[] args)
         {
             float num = 0;
-            int i = 0;
+            //int i = 0;
+            Array list[];
 
             while (num >= 0)
             {
-                Console.WriteLine("Type one number");
+                Console.WriteLine("Type one number. If you want to stop, type a negative number. ");
+                //num = Convert.ToInt16(Console.ReadLine());
                 num = Convert.ToInt16(Console.ReadLine());
 
-                num =
-                i++;
+                list = list + ", " + Convert.ToString(num);
             }
+
+            Console.WriteLine("Numbers: " + list);
 
         }
     }
---
+++ Ex_4-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ex_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.1. Crie um programa que crie um array com 8 posições para guardar a idade de pessoas.
+  
+            int size = 8; //definir o tamanho do array
+            int[] age = new int[size];
+
+            int i = 0; //contador pro while
+            int j = i + 1;
+
+            int sum = 0;
+            float mean = 0;
+
+            while (i < size)
+            {
+                // 4.1.1.Inicialize o array com um valor inválido como idade “-1”.
+                age[0] = -1;
+
+                // 4.1.2.Mostre que o array ficou corretamente inicializado.
+                //Console.WriteLine("The first element is: " + age[0]);
+
+                while (j < size)
+                {
+                    // 4.1.3.Preencha o array com 8 idades;
+                    int age_cons = 0;
+                    Console.WriteLine("Type one age: ");
+                    age_cons = Convert.ToInt16(Console.ReadLine());
+                    age[j] = age_cons;
+                    // 4.1.5.Calcule a média das idades do array;
+                    //mean = sum/length
+                    sum = sum + age[j];
+                    j++;
+                }
+
+                // 4.1.4.Mostre que o array ficou preenchido com as 8 idades introduzidas;
+                Console.WriteLine("Position: " + i + " Value: " + age[i]);            
+                i++;
+            }
+
+            mean = sum / size;
+            Console.WriteLine("\n\n The mean of ages is: " + mean);
+
+            
+
+            
+
+        }
+    }
+}
